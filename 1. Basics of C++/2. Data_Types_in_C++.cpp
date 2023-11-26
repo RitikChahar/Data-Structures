381c67f1e693f8b8179f4ea7abe7d2b6aa79238c
Ritik Chahar <ritickchahar@gmail.com>
2023-11-26 13:53:32 +0530

    Studied about basic data types in C++

---
+++ 1. Basics of C++/2. Data_Types_in_C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    long b = 1000000;
+    long long c = 100000000000;
+    float d = 10.44;
+    double e = 10.444444;
+    char f = 'R';
+    string g = "Ritik";
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+    cout<<g<<endl;
+}

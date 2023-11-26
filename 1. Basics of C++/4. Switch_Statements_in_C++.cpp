595ab62aa69026e231a181fb5acb8737e558f97f
Ritik Chahar <ritickchahar@gmail.com>
2023-11-26 14:11:19 +0530

    Studied Switch Statements in C++

---
+++ 1. Basics of C++/4. Switch_Statements_in_C++.cpp
@@ -0,0 +1,46 @@
+/*
+C++ program to print Monday if it's 1 and so on.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cin>>day;
+    switch (day)
+    {
+    case 1:
+        cout<<"It's Monday";
+        break;
+    
+    case 2:
+        cout<<"It's Tuesday";
+        break;
+    
+    case 3:
+        cout<<"It's Wednesday";
+        break;
+    
+    case 4:
+        cout<<"It's Thursday";
+        break;
+    
+    case 5:
+        cout<<"It's Friday";
+        break;
+    
+    case 6:
+        cout<<"It's Saturday";
+        break;
+    
+    case 7:
+        cout<<"It's Sunday";
+        break;
+    
+    default:
+        cout<<"Invalid Day Number";
+        break;
+    }
+
+    return 0;
+}
